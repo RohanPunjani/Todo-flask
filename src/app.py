b20d0d3d2711b6edd31159574c1749163b221f4c
RohanPunjani <rohanpunjani32@gmail.com>
2020-03-19 20:53:39 +0530

    First Commit

---
+++ src/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+
+        task_content = request.form['content']
+        newTask = Todo(content=task_content)
+
+        try:
+            db.session.add(newTask)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding a task"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Cant delete, got some issues"
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Cant update, got some issues"
+    else:
+        return render_template('update.html', task=task_to_update)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
